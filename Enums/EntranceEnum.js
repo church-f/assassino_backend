9bd0c08f165243d6cd426c806e989efb1443ac95
church-f <cristian.romano.game@gmail.com>
2026-01-09 12:04:19 +0100

    add: animazioni e avatar

--- Enums/EntranceEnum.js
+++ Enums/EntranceEnum.js
@@ -1,7 +1,10 @@
 const EntranceEnum = Object.freeze({
     0: 'Incendio',
-    1: "Fuochi d'artificio",
+    // 1: "Fuochi d'artificio",
     2: 'Soldi',
+    3: 'Coriandoli',
+    4: 'Pioggia',
+    5: 'Stelle cadenti'
 });
 
 module.exports = EntranceEnum;
--- Enums/avatarEnum.js
+++ Enums/avatarEnum.js
@@ -1,14 +1,17 @@
 const AvatarEnum = Object.freeze({
     0: 'Classico',
-    1: 'Vecchio',
-    2: 'Cieco',
-    3: 'Chiesa',
+    1: 'Coltello',
+    2: 'Pistola',
+    3: 'Labbra',
     4: 'Bambino',
     // 5: 'Coniglio',
     6: 'Corsa',
     7: 'Scii',
     8: 'Boccoli',
-    9: 'Nonna'
+    9: 'Nonna',
+    10: 'Vecchio',
+    11: 'Cieco',
+    12: 'Chiesa',
 });
 
 module.exports = AvatarEnum;
